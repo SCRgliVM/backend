e6c583e98179243fc1d906620656b687b9a85859
SCRgliVM <bogdan882456@gmail.com>
2023-03-07 22:29:24 +0200

    Implement basic REST

--- README.md
+++ README.md
@@ -1,8 +1,26 @@
-# Hello World web server
+# Simple Express web server
 
-It gives string 'Hello World!' as response
+Implement basic web-server functions
 
 Start web-server:
+
 ```sh
-node index.js
+npm start
 ```
+
+Start web-server with nodemon:
+
+```sh
+npm run dev
+```
+
+API:
+
+- `/`- hello world html-file
+- `/json` - random hardcored json garbage for testing pusposes
+- `/now` -return current date
+
+Server has basic request logs. All logs are available in stdout.
+Log format:
+
+- `METHOD URI - IP`
--- index.js
+++ index.js
@@ -1,9 +1,41 @@
-import * as http from "http";
+const express = require("express");
+const path = require("path");
 
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type' : 'text/plain'});
-    response.end('Hello World!');
+const app = express();
+
+let notes = [
+  { id: 1, content: "is easy" },
+  { id: 2, content: "avaScript" },
+  {
+    id: 3,
+    content: "GET and POST",
+  },
+];
+
+app.use("*", (req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl} - ${req.ip}`);
+  next();
 });
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app
+  .get("/", (request, response) => {
+    response.sendFile(path.join(__dirname, "public/index.html"));
+  })
+  .get("/json", (request, response) => {
+    response.json(notes);
+  })
+  .get(
+    "/now",
+    (request, response, next) => {
+      request.time = new Date().toString();
+      next();
+    },
+    (request, response) => {
+      response.json({ time: request.time });
+    }
+  );
 
 const PORT = 3001;
 app.listen(PORT);
